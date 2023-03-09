e298bbf64f3aff7a230e1f7ccfb07216fe580c95
jaglame <jaglame@gmail.com>
2023-03-09 16:05:32 -0300

    instalar

--- cliente/test_text.py
+++ cliente/test_text.py
@@ -10,13 +10,12 @@ from urllib.parse import urlencode
 HOST = "localhost"
 PORT = 9191
 
-
 data = open("sample_text.txt", "r")
 query = {"recurso": "HP-Deskjet-1010-series",
          "filename": "recurso.txt",
          "test": "false"}
 
-headers = {"CONTENT-TYPE": "text/plain; charset=utf-8"}
+headers = {"CONTENT-TYPE": "text/plain"}
 qstring = urlencode(query)
 response = requests.post("http://%s:%s?%s" % (HOST, PORT, qstring), data=data, headers=headers)
 
